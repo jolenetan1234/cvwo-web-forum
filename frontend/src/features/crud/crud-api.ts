d093ae41cd29338df73698657754667c397b7829
jolenetan1234 <jolneetanky@gmail.com>
2024-12-13 14:18:38 +0800

    WIP: PostCardDetails

--- frontend/src/features/crud/crud-api.ts
+++ frontend/src/features/crud/crud-api.ts
@@ -1,6 +1,6 @@
 import Post from "../../types/Post"
 
 // relevant API calls for CRUD
-export const getPostById = (id: number) => {
+export const getPostById = (postId: number) => {
     // return Post
 }
--- frontend/src/features/crud/crud-components.tsx
+++ frontend/src/features/crud/crud-components.tsx
@@ -1,12 +1,23 @@
 import { useParams } from "react-router-dom";
+import Post from "../../types/Post";
+import React from "react";
 
 // contains Card, and all the id-related stuff for one user only.
-export function PostCardDetails(): JSX.Element {
+export function PostCardDetails({ getPostById, Error }: { 
+    getPostById: (postId: number) => Post,
+    Error: React.FC }): JSX.Element {
     /**
-     * fetch Post
+     * Post = fetchPost();
      */
     const params = useParams<{ id : string }>();
     const postId = params.id;
+
+    // Handle case where postId is undefined
+    if (!postId) {
+        return <Error />;
+    }
+
+    const post = getPostById(parseInt(postId));
 
     return (
         <>PostCardDetail for post {postId}</>
