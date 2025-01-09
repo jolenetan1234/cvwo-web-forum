21d57827430c7066e3033c268d7bf12c3440a4d2
jolene <169898727+jolenetan1234@users.noreply.github.com>
2025-01-10 00:08:41 +0800

    [backend] implement login endpoint

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
+	"log"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/jolenetan1234/cvwo-web-forum/backend/app/controllers"
@@ -15,7 +19,24 @@ func init() {
 	initialisers.ConnectToDB()
 }
 
+func generateJWTSecret(length int) string {
+	// Create a byte slice of the desired length
+	secret := make([]byte, length)
+
+	// Fill the slice with cryptographically secure random bytes
+	_, err := rand.Read(secret)
+	if err != nil {
+		log.Println("[main.generateJWTSecret] Failed to generate JWT secret ", err)
+	}
+
+	// Encode the bytes to a base64 string
+	return base64.URLEncoding.EncodeToString(secret)
+}
+
 func main() {
+	secret := generateJWTSecret(32)
+	log.Println("HIIHI", secret)
+
 	r := gin.Default()
 
 	// Enable CORS for all origins (for local dev purposes)
@@ -33,16 +54,22 @@ func main() {
 
 	// Initialise repositories
 	var userRepo repositories.UserRepo = repositories.InitUserRepo(db)
+	var authRepo repositories.AuthRepo = repositories.InitAuthRepo(db)
 
 	// Initialise services
 	var userService services.UserService = services.InitUserService(userRepo)
+	var authService services.AuthService = services.InitAuthService(authRepo)
 
 	// Initialise controllers
 	var userController controllers.UserController = controllers.InitUserController(userService)
+	var authController controllers.AuthController = controllers.InitAuthController(authService)
 
 	// Routes
 	r.POST("/users", userController.CreateUser)
 	r.GET("/users/:id", userController.GetUserById)
+
+	// Auth
+	r.POST("login", authController.Login)
 
 	r.Run()
 
